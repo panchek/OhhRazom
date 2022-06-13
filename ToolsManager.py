dcb1d7830613eec7c5dbf7a46288b3ad3f7792b0
Egor <alex.pankov.new@gmail.com>
2022-06-14 00:21:44 +0300

    Changes 14_06_2022

--- ToolsManager.py
+++ ToolsManager.py
@@ -69,13 +69,13 @@ class ToolsManager(View):
                     wbAdmin[f'C{last_start + count}'] = i[1]['route']
 
                 strPath = "/home/acrzmcomua/public_html"
-                filename = strPath + f"media\OhhRazom\Excel\ManagerToolsRoutes\change_route_{datetime.datetime.now().strftime('%d_%m_%Y')}.xlsx"
+                filename = strPath + f"media/OhhRazom/Excel/ManagerToolsRoutes/change_route_{datetime.datetime.now().strftime('%d_%m_%Y')}.xlsx"
                 wb.save(filename=filename)
 
                 for i in df:
                     Totalplanes.objects.filter(Razom_number=i[0]).update(route=i[1])
 
-                return redirect(f"media\OhhRazom\Excel\ManagerToolsRoutes\change_route_{datetime.datetime.now().strftime('%d_%m_%Y')}.xlsx")
+                return redirect(f"media/OhhRazom/Excel/ManagerToolsRoutes/change_route_{datetime.datetime.now().strftime('%d_%m_%Y')}.xlsx")
 
         return HttpResponseRedirect(reverse_lazy('Autentificate'))
 
